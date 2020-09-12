74c9ed36a65f005e640d2a3c0522f24c14ae7512
wanting12345 <2571039622@qq.com>
2020-09-12 16:36:01 +0800

    commit

---
+++ CMake_test2/CMakeFiles/mathPowerDemo3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mathPowerDemo3.dir/main.cpp.o"
+  "mathPowerDemo3.pdb"
+  "mathPowerDemo3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mathPowerDemo3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMake_test2/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yevette/文档/git/CMake_test2/main.cpp" "/home/yevette/文档/git/CMake_test2/CMakeFiles/test.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "./menu"
+  "./student"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/yevette/文档/git/CMake_test2/menu/CMakeFiles/menu.dir/DependInfo.cmake"
+  "/home/yevette/文档/git/CMake_test2/student/CMakeFiles/student.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
